e8cc42d986bbfd63eabb35e6c0b79d72c00adb6a
Laxynium <grzesiu15432@gmail.com>
2023-01-18 23:30:11 +0100

    Single ship with single coordinate can be shot or missed

---
+++ src/Battleships.Console/Fleet.cs
@@ -0,0 +1,54 @@
+﻿namespace Battleships.Console;
+
+public record Coordinate(int X, int Y)
+{
+    public static implicit operator Coordinate((int x, int y) coordinate) =>
+        new(coordinate.x, coordinate.y);
+}
+
+public record FleetShip
+{
+    private readonly Coordinate _coordinate;
+
+    private FleetShip(Coordinate coordinate)
+    {
+        _coordinate = coordinate;
+    }
+    
+    public static FleetShip Create(Coordinate coordinate) => 
+        new(coordinate);
+
+    public Fleet.ShootResult ReceiveShot(Coordinate coordinate)
+    {
+        if (_coordinate == coordinate)
+        {
+            return Fleet.ShootResult.GotHit;
+        }
+
+        return Fleet.ShootResult.Miss;
+    }
+}
+
+public record Fleet
+{
+    private readonly FleetShip _ship;
+
+    private Fleet(FleetShip ship)
+    {
+        _ship = ship;
+    }
+
+    public static Fleet Create(FleetShip ship) => 
+        new(ship);
+
+    public ShootResult ReceiveShot(Coordinate coordinate)
+    {
+        return _ship.ReceiveShot(coordinate);
+        
+    }
+
+    public enum ShootResult
+    {
+        GotHit, Miss
+    }
+}
---
+++ src/Battleships.UnitTests/ShootingFleetTests.cs
@@ -0,0 +1,30 @@
+﻿using Battleships.Console;
+using FluentAssertions;
+
+namespace Battleships.UnitTests;
+
+public class ShootingFleetTests
+{
+    [Fact]
+    public void for_single_coordinate_ship_there_is_hit_when_shot_coordinates_matches_ship_coordinate()
+    {
+        Fleet.Create(FleetShip.Create((3, 3)))
+            .ReceiveShot((3, 3)).Should().Be(Fleet.ShootResult.GotHit);
+        
+        Fleet.Create(FleetShip.Create((4, 4)))
+            .ReceiveShot((4, 4)).Should().Be(Fleet.ShootResult.GotHit);
+        
+        Fleet.Create(FleetShip.Create((5, 7)))
+            .ReceiveShot((5, 7)).Should().Be(Fleet.ShootResult.GotHit);
+    }
+
+    [Fact]
+    public void for_single_coordinate_ship_there_is_miss_when_shot_coordinates_is_not_matching_ship_coordinate()
+    {
+        var fleet = Fleet.Create(FleetShip.Create((3, 3)));
+
+        fleet.ReceiveShot((3, 4)).Should().Be(Fleet.ShootResult.Miss);
+        fleet.ReceiveShot((4, 3)).Should().Be(Fleet.ShootResult.Miss);
+        fleet.ReceiveShot((4, 4)).Should().Be(Fleet.ShootResult.Miss);
+    }
+}
