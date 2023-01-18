61182cf3af9539bff3af8c43f297003f99930cb9
Laxynium <grzesiu15432@gmail.com>
2023-01-18 22:16:05 +0100

    List tests for creating the ocean grid

---
+++ src/Battleships.UnitTests/CreatingOceanGridTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Battleships.UnitTests;
+
+public class CreatingOceanGridTests
+{
+    public void can_be_created(){}
+    
+    public void cannot_have_negative_or_0_size(){}
+    
+    public void cannot_be_created_without_ships(){}
+    
+    public void cannot_be_created_when_there_are_ships_collisions(){}
+    
+    public void cannot_be_created_when_there_are_ships_outside_boundaries(){}
+}
