9a771ecd04bf8fd897851d9c57953c60b26616c3
Laxynium <grzesiu15432@gmail.com>
2023-01-19 23:29:57 +0100

    Move game states from test to src project

---
+++ src/Battleships.Console/GameStates.cs
@@ -0,0 +1,40 @@
+﻿using Battleships.Console.Fleets;
+
+namespace Battleships.Console;
+
+public record TakeAShotAt(Coordinate Coordinate);
+
+public interface IGameState
+{
+    IGameState HandleChange(TakeAShotAt takeAShotAt);
+}
+
+public class PlayerTurnState : IGameState
+{
+    private readonly Fleet _fleet;
+
+    public PlayerTurnState(Fleet fleet)
+    {
+        _fleet = fleet;
+    }
+
+    public IGameState HandleChange(TakeAShotAt takeAShotAt)
+    {
+        var result = _fleet.ReceiveShot(takeAShotAt.Coordinate);
+        
+        if (result == ShootResult.FleetSunk)
+        {
+            return new GameOverState();
+        }
+        
+        return new PlayerTurnState(_fleet);
+    }
+}
+
+public class GameOverState : IGameState
+{
+    public IGameState HandleChange(TakeAShotAt takeAShotAt)
+    {
+        return new GameOverState();
+    }
+}
--- src/Battleships.UnitTests/SoloMode/GameStatesChangesTests.cs
+++ src/Battleships.UnitTests/SoloMode/GameStatesChangesTests.cs
@@ -1,4 +1,5 @@
-﻿using Battleships.Console.Fleets;
+﻿using Battleships.Console;
+using Battleships.Console.Fleets;
 using FluentAssertions;
 
 namespace Battleships.UnitTests.SoloMode;
@@ -61,42 +62,5 @@ public class GameStatesChangesTests
         
         state = state.HandleChange(new TakeAShotAt((2, 2)));
         state.Should().BeOfType<GameOverState>();
-    }
-}
-
-public record TakeAShotAt(Coordinate Coordinate);
-
-public interface IGameState
-{
-    IGameState HandleChange(TakeAShotAt takeAShotAt);
-}
-
-public class PlayerTurnState : IGameState
-{
-    private readonly Fleet _fleet;
-
-    public PlayerTurnState(Fleet fleet)
-    {
-        _fleet = fleet;
-    }
-
-    public IGameState HandleChange(TakeAShotAt takeAShotAt)
-    {
-        var result = _fleet.ReceiveShot(takeAShotAt.Coordinate);
-        
-        if (result == ShootResult.FleetSunk)
-        {
-            return new GameOverState();
-        }
-        
-        return new PlayerTurnState(_fleet);
-    }
-}
-
-public class GameOverState : IGameState
-{
-    public IGameState HandleChange(TakeAShotAt takeAShotAt)
-    {
-        return new GameOverState();
     }
 }
