b28f1ea51c4ae713052231d1e0fbf2ba115ae7e5
Laxynium <grzesiu15432@gmail.com>
2023-01-22 13:35:59 +0100

    Remove possibility of creating FleetShip without id

--- src/Battleships.Console/Fleets/FleetShip.cs
+++ src/Battleships.Console/Fleets/FleetShip.cs
@@ -2,23 +2,15 @@ namespace Battleships.Console.Fleets;
 
 public record FleetShip
 {
-    private FleetShipId _id = new("1");
+    private readonly FleetShipId _id;
     private readonly Dictionary<Coordinates, bool> _holes;
 
-    private FleetShip(IEnumerable<Coordinates> coordinates) => 
-        _holes = coordinates.ToDictionary(x=>x, _=> false);
-    
     private FleetShip(FleetShipId id, IEnumerable<Coordinates> coordinates) => 
         (_id, _holes) = (id, coordinates.ToDictionary(x=>x, _=> false));
 
     public CoordinatesSet CoordinatesSet =>
         CoordinatesSet.Create(_holes.Keys.First(), _holes.Keys.Skip(1).ToArray());
 
-    public static FleetShip Create(CoordinatesSet coordinatesSet)
-    {
-        return new FleetShip(coordinatesSet.Set);
-    }
-    
     public static FleetShip Create(FleetShipId id, CoordinatesSet coordinatesSet)
     {
         return new FleetShip(id, coordinatesSet.Set);
