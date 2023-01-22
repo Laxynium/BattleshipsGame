0cef349572a15b9cb1fc09499e30be4c1fe417c3
Laxynium <grzesiu15432@gmail.com>
2023-01-22 23:09:20 +0100

    Add ship blueprint tests

---
+++ src/Battleships.UnitTests/MatchConfigurations/ShipBlueprintTests.cs
@@ -0,0 +1,28 @@
+﻿using Battleships.Console.MatchConfigurations;
+using FluentAssertions;
+using static Battleships.UnitTests.Builders.CoordinatesSetBuilder;
+namespace Battleships.UnitTests.MatchConfigurations;
+
+public class ShipBlueprintTests
+{
+    [Fact]
+    public void error_when_there_are_negative_coordinates()
+    {
+        var action = () => ShipBlueprint.Create(CreateCoordinatesSet((-1, 0), (0, 0)));
+        action.Should().Throw<Exception>().WithMessage("*>= 0*");
+    }
+    
+    [Fact]
+    public void error_when_coords_are_not_relative_to_0_0_origin()
+    {
+        var action = () => ShipBlueprint.Create(CreateCoordinatesSet((1, 1), (2, 1)));
+        action.Should().Throw<Exception>().WithMessage("*relative*origin*");
+    }
+
+    [Fact]
+    public void can_be_created()
+    {
+        var action = () => ShipBlueprint.Create(CreateCoordinatesSet((0, 1), (0, 0)));
+        action.Should().NotThrow();
+    }
+}
