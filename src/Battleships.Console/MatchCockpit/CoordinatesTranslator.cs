977abe952c0c4fc7c914ab3995247fed99e5bbcf
Laxynium <grzesiu15432@gmail.com>
2023-01-22 16:02:24 +0100

    Add coordinates translator

---
+++ src/Battleships.Console/MatchCockpit/CoordinatesTranslator.cs
@@ -0,0 +1,20 @@
+﻿using Battleships.Console.Fleets;
+
+namespace Battleships.Console.MatchCockpit;
+
+public static class CoordinatesTranslator
+{
+    public static Coordinates AFleetCoordinates(string gridCoordinates)
+    {
+        var row = gridCoordinates[0]-'A';
+        var column = gridCoordinates[1]-'1';
+        return new Coordinates(column, row);
+    }
+
+    public static string AGridCoordinates(Coordinates fleetCoordinates)
+    {
+        var (column, row) = fleetCoordinates;
+
+        return $"{(char)('A'+row)}{(column+1).ToString()}";
+    }
+}
---
+++ src/Battleships.UnitTests/MatchCockpit/CoordinatesTranslateTests.cs
@@ -0,0 +1,28 @@
+﻿using Battleships.Console.Fleets;
+using Battleships.Console.MatchCockpit;
+using FluentAssertions;
+
+namespace Battleships.UnitTests.MatchCockpit;
+
+public class CoordinatesTranslateTests
+{
+    [Fact]
+    public void translates_fleet_coordinates_to_grid_one()
+    {
+        var fleetCoords = new Coordinates(4, 5);
+
+        var gridCoords = CoordinatesTranslator.AGridCoordinates(fleetCoords);
+
+        gridCoords.Should().BeEquivalentTo("F5");
+    }
+    
+    [Fact]
+    public void translates_grid_coords_to_fleet_one()
+    {
+        var gridCords = "D7";
+
+        var fleetCords = CoordinatesTranslator.AFleetCoordinates(gridCords);
+
+        fleetCords.Should().Be(new Coordinates(6, 3));
+    }
+}
