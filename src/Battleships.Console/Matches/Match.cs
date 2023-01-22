51e4a2ac0197b93544d0bbae16142ae1e40aeaec
Laxynium <grzesiu15432@gmail.com>
2023-01-22 12:04:38 +0100

    Shooting a target always results in success

---
+++ src/Battleships.Console/Matches/Match.cs
@@ -0,0 +1,22 @@
+﻿using Battleships.Console.Fleets;
+using CSharpFunctionalExtensions;
+
+namespace Battleships.Console.Matches;
+
+public interface IMatchEvent
+{
+}
+
+public interface IMatchCommand
+{
+}
+
+public sealed record ShootATarget(Coordinates Coordinates) : IMatchCommand;
+
+public class Match
+{
+    public Result<IReadOnlyCollection<IMatchEvent>> Handle(IMatchCommand command)
+    {
+        return Result.Success<IReadOnlyCollection<IMatchEvent>>(new List<IMatchEvent>());
+    }
+}
---
+++ src/Battleships.UnitTests/Matches/MatchesTests.cs
@@ -0,0 +1,17 @@
+﻿using Battleships.Console.Matches;
+using FluentAssertions;
+
+namespace Battleships.UnitTests.Matches;
+
+public class MatchesTests
+{
+    [Fact]
+    public void initial_state_allows_to_shoot_a_selected_target()
+    {
+        var match = new Match();
+
+        var result = match.Handle(new ShootATarget((0, 0)));
+
+        result.IsSuccess.Should().BeTrue();
+    }
+}
