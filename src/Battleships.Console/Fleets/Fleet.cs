bd2dab72f370aae9c8f0110a054c653b0dd5a9b3
Laxynium <grzesiu15432@gmail.com>
2023-01-22 13:31:29 +0100

    Simplify if statements

--- src/Battleships.Console/Fleets/Fleet.cs
+++ src/Battleships.Console/Fleets/Fleet.cs
@@ -4,7 +4,7 @@ public record Fleet
 {
     private readonly List<FleetShip> _ships;
 
-    private Fleet(IEnumerable<FleetShip> ships) => 
+    private Fleet(IEnumerable<FleetShip> ships) =>
         _ships = ships.ToList();
 
     public static Fleet Create(FleetShip ship, params FleetShip[] ships)
@@ -28,14 +28,15 @@ public record Fleet
         foreach (var ship in _ships)
         {
             var result = ship.ReceiveShot(coordinates);
-            if (result is ShootResult.Hit hit)
+
+            if (result is ShootResult.Miss)
             {
-                return IsFleetSunk() ? new ShootResult.FleetSunk(hit.FleetShipId) : result;
+                continue;
             }
-            if (result is ShootResult.Sunk sunk)
-            {
-                return IsFleetSunk() ? new ShootResult.FleetSunk(sunk.FleetShipId) : result;
-            }
+
+            return IsFleetSunk()
+                ? new ShootResult.FleetSunk(GetFleetShipId(result))
+                : result;
         }
 
         return new ShootResult.Miss();
@@ -46,9 +47,18 @@ public record Fleet
         return _ships.All(x => x.IsSunk());
     }
 
+    private static FleetShipId GetFleetShipId(ShootResult shootResult) =>
+        shootResult switch
+        {
+            ShootResult.Hit (var id) => id,
+            ShootResult.Sunk (var id) => id,
+            ShootResult.FleetSunk (var id) => id,
+            _ => throw new ArgumentOutOfRangeException(nameof(shootResult), shootResult, null)
+        };
 
-    private static IEnumerable<(T x, T y)> CartesianProduct<T>(IList<T> items) => 
-        from x in items 
-        from y in items 
+
+    private static IEnumerable<(T x, T y)> CartesianProduct<T>(IList<T> items) =>
+        from x in items
+        from y in items
         select (x, y);
 }
