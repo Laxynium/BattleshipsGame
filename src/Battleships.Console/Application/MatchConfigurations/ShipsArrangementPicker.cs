ad9f3b3600550ea9c99bb7209409a8fb3d563d84
Laxynium <grzesiu15432@gmail.com>
2023-01-24 18:04:11 +0100

    Add picking from generated ships arrangements

---
+++ src/Battleships.Console/Application/MatchConfigurations/ShipsArrangementPicker.cs
@@ -0,0 +1,94 @@
+﻿using Battleships.Console.Application.Fleets;
+
+namespace Battleships.Console.Application.MatchConfigurations;
+
+public static class ShipsArrangementPicker
+{
+    public delegate CoordinatesSet ShipArrangementPicker(IReadOnlyList<CoordinatesSet> shipArrangements);
+    public delegate IReadOnlyCollection<CoordinatesSet> ShipArrangementsProvider(int shipIndex);
+    public static CoordinatesSet[] PickShipsArrangement(CoordinatesSet[] ships, ShipArrangementPicker shipArrangementPicker, ShipArrangementsProvider shipArrangementsProvider)
+    {
+        var stack = new Stack<Node>();
+        
+        var currentNode = Node.CreateRootNode();
+        stack.Push(currentNode);
+
+        while (stack.Count > 0)
+        {
+            currentNode = stack.Peek();
+            
+            var notOverlappingShipArrangements = currentNode.GetNotOverlappingShipArrangements(shipArrangementsProvider);
+
+            if (notOverlappingShipArrangements.Count == 0)
+            {
+                currentNode.SelfExcludeFromParent();
+
+                stack.Pop();
+                continue;
+            }
+            
+            var shipArrangement = shipArrangementPicker(notOverlappingShipArrangements);
+            
+            var nextNode = Node.Create(currentNode, shipArrangement);
+
+            if (nextNode.ShipsArrangement.Length == ships.Length)
+            {
+                return nextNode.ShipsArrangement;
+            }
+            
+            stack.Push(nextNode);
+        }
+
+        return currentNode.ShipsArrangement;
+    }
+    
+    private class Node
+    {
+        private readonly List<CoordinatesSet> _excluded = new();
+        private readonly Node? _parent;
+        private readonly CoordinatesSet? _currentShipArrangement;
+        private readonly int _currentShipIndex;
+
+        public CoordinatesSet[] ShipsArrangement { get; }
+
+        private Node(Node? parent, CoordinatesSet? currentShipArrangement, CoordinatesSet[] shipsArrangement, int currentShipIndex)
+        {
+            _parent = parent;
+            ShipsArrangement = shipsArrangement;
+            _currentShipArrangement = currentShipArrangement;
+            _currentShipIndex = currentShipIndex;
+        }
+        
+        public static Node Create(Node parent, CoordinatesSet currentShipArrangement) => 
+            new(parent, 
+                currentShipArrangement, 
+                parent.ShipsArrangement.Concat(new[] { currentShipArrangement }).ToArray(), 
+                parent._currentShipIndex+1);
+
+        public static Node CreateRootNode() => new(
+            null, 
+            null, 
+            Array.Empty<CoordinatesSet>(),
+            0);
+
+        public List<CoordinatesSet> GetNotOverlappingShipArrangements(
+            ShipArrangementsProvider shipArrangementsProvider)
+        {
+            var result = shipArrangementsProvider(_currentShipIndex)
+                .Where(x => !_excluded.Contains(x))
+                .Where(x => !CoordinatesSet.AreSomeOverlapping(
+                    ShipsArrangement.Concat(new[] { x }).ToArray()))
+                .ToList();
+            
+            return result;
+        }
+
+        public void SelfExcludeFromParent()
+        {
+            if (_currentShipArrangement is not null)
+            {
+                _parent?._excluded.Add(_currentShipArrangement);
+            }
+        }
+    }
+}
---
+++ src/Battleships.UnitTests/MatchConfigurations/ShipsArrangementPickerTests.cs
@@ -0,0 +1,137 @@
+﻿using Battleships.Console.Application.Fleets;
+using Battleships.Console.Application.MatchConfigurations;
+using FluentAssertions;
+
+namespace Battleships.UnitTests.MatchConfigurations;
+
+public class ShipsArrangementPickerTests
+{
+    [Fact]
+    public void for_generated_ships_arrangements_picks_arrangement_consisting_of_valid_numbers_of_ships_on_2x4_grid()
+    {
+        var ships = new List<CoordinatesSet>
+        {
+            ShipBlueprint.FromText("a", "--").Set,
+            ShipBlueprint.FromText("a", "---").Set
+        }.ToArray();
+        var constrains = new GridConstrains(2, 4);
+        var possibleShipsArrangements = GeneratedShipsArrangements(ships, constrains);
+
+        var result =
+            ShipsArrangementPicker.PickShipsArrangement(ships, AlwaysTakeFirstOne, i => possibleShipsArrangements[i]);
+
+        result.Should().HaveCount(2);
+        result.Should().Contain(x => x.Set.Count == 2);
+        result.Should().Contain(x => x.Set.Count == 3);
+    }
+
+    [Fact]
+    public void for_generated_ships_arrangements_picks_arrangement_consisting_of_valid_numbers_of_ships_on_1x4_grid()
+    {
+        var ships = new List<CoordinatesSet>
+        {
+            ShipBlueprint.FromText("a", "--").Set,
+            ShipBlueprint.FromText("a", "---").Set
+        }.ToArray();
+
+        var constrains = new GridConstrains(1, 4);
+        var possibleShipsArrangements = GeneratedShipsArrangements(ships, constrains);
+
+        var result =
+            ShipsArrangementPicker.PickShipsArrangement(ships, AlwaysTakeFirstOne, i => possibleShipsArrangements[i]);
+
+        result.Should().HaveCount(0);
+    }
+
+    [Fact]
+    public void when_there_are_many_possible_arrangements_to_choose_from()
+    {
+        var ships = new List<CoordinatesSet>
+        {
+            ShipBlueprint.FromText("a", "--").Set,
+            ShipBlueprint.FromText("a", "--").Set
+        }.ToArray();
+        var constrains = new GridConstrains(10, 10);
+        var possibleShipsArrangements = GeneratedShipsArrangements(ships, constrains);
+
+        var result =
+            ShipsArrangementPicker.PickShipsArrangement(ships, AlwaysTakeFirstOne, i => possibleShipsArrangements[i]);
+
+        result.Should().HaveCount(2);
+    }
+
+    [Fact]
+    public void when_there_are_many_not_overlapping_choices()
+    {
+        var ships = new List<CoordinatesSet>
+        {
+            ShipBlueprint.FromText("a", "---").Set,
+            ShipBlueprint.FromText("a", "--").Set
+        }.ToArray();
+        var constrains = new GridConstrains(2, 3);
+        var possibleShipsArrangements = new[]
+        {
+            new[]
+            {
+                CoordinatesSet.Create((0, 0), (0, 1), (0, 2)),
+                CoordinatesSet.Create((1, 0), (1, 1), (1, 2))
+            },
+            new[]
+            {
+                CoordinatesSet.Create((0, 0), (1, 0)),
+                CoordinatesSet.Create((0, 1), (1, 1)),
+                CoordinatesSet.Create((0, 2), (1, 2)),
+
+                CoordinatesSet.Create((0, 0), (0, 1)),
+                CoordinatesSet.Create((0, 1), (0, 2)),
+
+                CoordinatesSet.Create((1, 0), (1, 1)),
+                CoordinatesSet.Create((1, 1), (1, 2))
+            }
+        };
+        var picks = new[] { new[]{0, 1}, new []{ 0, 1}, new []{0, 1}, new []{ 0, 3} };
+
+        var picker = CreatePickerFrom(picks);
+
+        var result = ShipsArrangementPicker.PickShipsArrangement(ships,
+            picker,
+            i => possibleShipsArrangements[i]);
+
+        result.Should().ContainInOrder(new[]
+        {
+            CoordinatesSet.Create((0, 0), (0, 1), (0, 2)),
+            CoordinatesSet.Create((1, 1), (1, 2))
+        });
+    }
+
+    private static ShipsArrangementPicker.ShipArrangementPicker CreatePickerFrom(int[][] picks)
+    {
+        var firstIndex = 0;
+        var secondIndex = 0;
+        return (shipArrangements) =>
+        {
+            var result = shipArrangements[picks[firstIndex][secondIndex]];
+            if (secondIndex >= picks[secondIndex].Length-1)
+            {
+                firstIndex++;
+                secondIndex = 0;
+            }
+            else
+            {
+                secondIndex++;
+            }
+            
+            return result;
+        };
+    }
+
+    private static IReadOnlyCollection<CoordinatesSet>[] GeneratedShipsArrangements(CoordinatesSet[] ships,
+        GridConstrains constrains) =>
+        ships
+            .Select(x => ArrangementsGenerator.GenerateFor(x, constrains))
+            .ToArray();
+
+
+    private static CoordinatesSet AlwaysTakeFirstOne(IReadOnlyList<CoordinatesSet> actualShipArrangements) =>
+        actualShipArrangements.First();
+}
